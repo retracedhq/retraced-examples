9ed999febf6311ff8b63d4dae6f887546ebcf4d9
ukrocks007 <ukrocks.mehta@gmail.com>
2023-07-12 18:27:52 +0530

    fixes

--- apps/next-auth/components/audit-log-viewer.tsx
+++ apps/next-auth/components/audit-log-viewer.tsx
@@ -5,79 +5,59 @@ export default function AccessDenied() {
   const [logs, setLogs] = useState([])
   var lastStartTime = 0
   useEffect(() => {
-    getLogs()
-    setInterval(() => {
-      getLogs()
-    }, 2500)
+    worker()
   }, [])
-  const getLogs = () => {
-    axios
-      .get(
-        `/api/auditLogs/get?startTime=${+new Date(
-          new Date().setDate(new Date().getDate() - 1)
-        )}&endTime=${+new Date(new Date().setDate(new Date().getDate() + 1))}`
-      )
-      .then((res) => {
-        const events: [] = res.data.events
-        if (Array.isArray(events)) {
-          if (lastStartTime == 0) {
-            if (events.length > 0) {
-              const temp = events
-                .filter((e) => e["created"])
-                .reverse()
-                .slice(0, 20)
-              setLogs(temp)
-              lastStartTime = parseInt(temp[0]["created"])
-            }
-          } else {
-            let tmp: any[] = events.reverse()
-            if (tmp.length > 0) {
-              let final: never[] = tmp
-                .filter((e) => e && e.created)
-                .map((t) => {
-                  if (parseInt(t["created"].toString()) > lastStartTime) {
-                    return { ...t, is_new: true }
-                  } else {
-                    return { ...t, is_new: false }
-                  }
-                }) as never[]
-              lastStartTime = parseInt(final[0]["created"])
-              setLogs(final.slice(0, 20))
+
+  const worker = async () => {
+    setInterval(async () => {
+      await getLogs()
+    }, 5000)
+  }
+
+  const getLogs = async () => {
+    return new Promise((resolve) => {
+      axios
+        .get(
+          `/api/auditLogs/get?startTime=${+new Date(
+            new Date().setDate(new Date().getDate() - 1)
+          )}&endTime=${+new Date(new Date().setDate(new Date().getDate() + 1))}`
+        )
+        .then((res) => {
+          const events: [] = res.data
+          if (Array.isArray(events)) {
+            if (lastStartTime == 0) {
+              if (events.length > 0) {
+                const temp = events.filter((e) => e["created"]).slice(0, 20)
+                setLogs(temp)
+                lastStartTime = +new Date(temp[0]["created"])
+              }
+            } else {
+              let tmp: any[] = events
+              if (tmp.length > 0) {
+                let final: never[] = tmp
+                  .filter((e) => e && e.created)
+                  .map((t) => {
+                    if (+new Date(t["created"].toString()) > lastStartTime) {
+                      return { ...t, is_new: true }
+                    } else {
+                      return { ...t, is_new: false }
+                    }
+                  }) as never[]
+                lastStartTime = +new Date(final[0]["created"])
+                setLogs(final.slice(0, 20))
+                resolve(true)
+              }
             }
           }
-        }
-      })
+        })
+        .catch(() => {
+          resolve(true)
+        })
+    })
   }
 
   const formatEvent = (input: string) => {
-    let found = false
-    do {
-      if (input.indexOf("**") != -1) {
-        if (!found) {
-          input = input.replace("**", "")
-          found = true
-        } else {
-          input = input.replace("**", "")
-          found = false
-        }
-      } else {
-        return input
-      }
-    } while (true)
-  }
-
-  const getMetaString = (obj: any, crud: string): string[] => {
-    switch (crud) {
-      case "u":
-        const updates = JSON.parse(obj.updates)
-        return Object.keys(updates).map((k) => {
-          return `Updated ${k}: ${updates[k]}`
-        })
-      case "d":
-        return [`Expense for ${obj["title"]} deleted!`]
-      default:
-        return []
-    }
+    return input.replaceAll("**", "")
   }
 
   return (
@@ -88,7 +68,11 @@ export default function AccessDenied() {
           return (
             <div
               key={i}
-              style={{ border: "2px solid grey", marginBottom: "10px" }}
+              style={{
+                border: "2px solid grey",
+                marginBottom: "10px",
+                fontWeight: "bold",
+              }}
             >
               <div
                 style={{
@@ -97,21 +81,6 @@ export default function AccessDenied() {
                 }}
               >
                 <span>{formatEvent(l["display"]["markdown"])}</span>
-                <br />
-                {Object.keys(l["fields"] || {}).length > 0 && (
-                  <>
-                    <br />
-                    <span>
-                      {getMetaString(l["fields"], l["crud"]).map((m) => {
-                        return (
-                          <div>
-                            <u>{m}</u>
-                          </div>
-                        )
-                      })}
-                    </span>
-                  </>
-                )}
               </div>
             </div>
           )
--- apps/next-auth/pages/api/auditLogs/get.ts
+++ apps/next-auth/pages/api/auditLogs/get.ts
@@ -1,33 +1,93 @@
-export default function handler(req: any, res: any) {
-  var myHeaders = new Headers()
-  myHeaders.append("Authorization", "token=dev")
-  myHeaders.append("Content-Type", "application/json")
-
-  var raw = JSON.stringify({
-    query: {
-      search_text: "",
-      offset: 0,
-      length: 1000,
-      start_time: req.body.startTime,
-      end_time: req.body.endTime,
-      create: "c",
-      read: "r",
-      update: "u",
-      delete: "d",
-    },
-  })
-
-  var requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
+export default async function handler(req: any, res: any) {
+  try {
+    const token = await getViewerToken()
+    const session = await getViewerSession(token)
+    const logs = await getAuditLogs(session)
+    res.status(200).send(logs)
+  } catch (ex) {
+    res.status(400).json(ex)
   }
+}
 
-  return fetch(
-    "http://localhost:3000/auditlog/admin/v1/project/dev/events/search?environment_id=dev",
-    requestOptions
-  )
-    .then((response) => response.text())
-    .then((result) => res.status(200).send(result))
-    .catch((error) => res.status(400).send(error))
+const getViewerToken = async () => {
+  try {
+    var myHeaders = new Headers()
+    myHeaders.append("Authorization", "Token token=dev")
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    } as any
+
+    const resp = await fetch(
+      "http://localhost:3000/auditlog/publisher/v1/project/dev/viewertoken?group_id=retraced-demo&actor_id=admin&isAdmin=true",
+      requestOptions
+    )
+    const json = await resp.json()
+    return json.token
+  } catch (ex) {
+    console.log(ex)
+  }
+}
+
+const getViewerSession = async (token: string) => {
+  try {
+    var myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+
+    var raw = JSON.stringify({
+      token,
+    })
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    } as any
+
+    const res = await fetch(
+      "http://localhost:3000/auditlog/viewer/v1/viewersession",
+      requestOptions
+    )
+    const json = await res.json()
+    return json.token
+  } catch (ex) {
+    console.log(ex)
+  }
+}
+
+const getAuditLogs = async (session: string) => {
+  try {
+    var myHeaders = new Headers()
+    myHeaders.append("Authorization", session)
+    myHeaders.append("Content-Type", "application/json")
+
+    var raw = JSON.stringify({
+      query:
+        "\n            query Search($query: String!, $last: Int, $before: String) {\n              search(query: $query, last: $last, before: $before) {\n                totalCount\n                pageInfo {\n                  hasPreviousPage\n                }\n                edges {\n                  cursor\n                  node {\n                    id\n                    action\n                    crud\n                    created\n                    received\n                    canonical_time\n                    description\n                    actor {\n                      id\n                      name\n                      href\n                    }\n                    group {\n                      id\n                      name\n                    }\n                    target {\n                      id\n                      name\n                      href\n                      type\n                    }\n                    display {\n                      markdown\n                    }\n                    is_failure\n                    is_anonymous\n                    source_ip\n                    country\n                    loc_subdiv1\n                    loc_subdiv2\n                  }\n                }\n              }\n            }\n          ",
+      variables: {
+        query: "crud:c,u,d",
+        last: 20,
+        before: "",
+      },
+    })
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    } as any
+
+    const res = await fetch(
+      "http://localhost:3000/auditlog/viewer/v1/graphql",
+      requestOptions
+    )
+    const json = await res.json()
+    return json.data.search.edges.map((d: any) => d.node)
+  } catch (ex) {
+    return []
+  }
 }
